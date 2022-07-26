97ded9353e265f8bb9d696472000f5bbaee32546
aserrate <aserrate1712@hotmail.com>
2022-07-26 17:35:11 -0500

    Estudiando Seccion4-cap41

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
+import { App } from './componentes/HelloWorld'
+import { PrimerComponente } from './componentes/FirstComponent'
 
-function App() {
-    return <h1>Hola mundo...</h1>;
-}
+//Estilo de la APP
+import './estilos/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    //Props=Propiedades de los componentes, que se pueden desestructurar y enviar como parametros en los componentes cuando se requiera
     <React.StrictMode>
         <App />
+        <PrimerComponente title="Props titlulo" subtitle="subtitulo"/>
     </React.StrictMode>
 );
