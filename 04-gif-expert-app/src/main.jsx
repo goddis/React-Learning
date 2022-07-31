061da0504fcb77198913fe8b58c5f88b34429e69
aserrate <aserrate1712@hotmail.com>
2022-07-31 17:52:38 -0500

    Se termina la seccion 6

--- 04-gif-expert-app/src/main.jsx
+++ 04-gif-expert-app/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { GifExpertApp } from './componentes/GifExpertApp'
-import './estilos/styles.css'
+import { GifExpertApp } from './components/GifExpertApp'
+import './styles/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //  <React.StrictMode> Este no se usa en producción
   <React.StrictMode>
     <GifExpertApp />
   </React.StrictMode>
